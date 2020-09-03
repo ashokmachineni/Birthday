1d9717010ad1622615c899a1c0c5a2da5daacc9d
ashokmachineni <ashok5679m@gmail.com>
2020-09-03 11:57:59 +0100

    initial setup with firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyD_fIyWvCy8Jik7e6t5lNsMLWLW8Y2ka5M",
+  authDomain: "birthday-6d7ce.firebaseapp.com",
+  databaseURL: "https://birthday-6d7ce.firebaseio.com",
+  projectId: "birthday-6d7ce",
+  storageBucket: "birthday-6d7ce.appspot.com",
+  messagingSenderId: "196539864139",
+  appId: "1:196539864139:web:f904aa845f3bf5c0ad29ce"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
